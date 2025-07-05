361d189ea81fc6e9ec92bf54bd51c468b20a9778
ant88.gregoire <ant88.gregoire@gmail.com>
2025-07-05 15:48:46 +0200

    provisioning servers

--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ USER=$2
 KEY=$3
 
 echo "[servers]" > ansible/inventory.ini
-echo "$USER ansible_host=$IP ansible_user=$USER ansible_ssh_private_key_file=$KEY ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> ansible/inventory.ini
+echo "$USER ansible_host=$IP ansible_user=$USER ansible_ssh_private_key_file=~/provisioning/$KEY ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> ansible/inventory.ini
 
 cd ansible
 ansible-playbook -i inventory.ini playbook.yml
